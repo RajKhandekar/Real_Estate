a553a90a0239aa784079174993113596bf6b9e82
RajKhandekar <rajkhandekar136@gmail.com>
2024-06-30 23:38:02 +0530

    first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,25 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best Interest Rates on the Market",
+    detail:
+      "Get the best interest rates available on the market. Our rates are competitive, ensuring you get the maximum return on your investment with minimum risk.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent Unstable Prices",
+    detail:
+      "We help you avoid the pitfalls of unstable prices. Our strategies are designed to maintain price stability, providing you with peace of mind and financial security.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best Prices on the Market",
+    detail:
+      "We offer the best prices in the market. Our competitive pricing ensures you get the most value for your money, whether you're buying or selling.",
+  },
+];
+
+export default data;
